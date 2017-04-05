107886f10567e2280e6f61d6a91e2a5d14415654
Tim Brown <tim.brown@bbc.co.uk>
2017-04-05 20:47:20 +0100

    add bin/filter-gutenberg.py - intial version of a python script to normalise the Gutenberg version

---
+++ bin/filter-gutenberg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import fileinput
+import re
+
+for line in fileinput.input():
+    # have we got an empty line? then move on
+    if re.match('^\s*$', line):
+        continue
+
+    # remove any trailing whitespace, e.g. newline characters
+    line = line.rstrip()
+
+    # deal with abbreviated character names
+    line = line.replace('Fran.', 'FRANCISCO')
+    line = line.replace('Ber.','BERNARDO')
+
+    print(line)
+

930d657686f78df204ab160ad2619d199364647c
rosemadr <madi@redpill.me.uk>
2017-04-09 14:27:41 +0100

    added replacements for Mar. and Hor.

--- bin/filter-gutenberg.py
+++ bin/filter-gutenberg.py
@@ -14,6 +14,9 @@ for line in fileinput.input():
     # deal with abbreviated character names
     line = line.replace('Fran.', 'FRANCISCO')
     line = line.replace('Ber.','BERNARDO')
+    line = line.replace('Mar.','MARCELLUS')
+    line = line.replace('Hor.','HORATIO')
+    
 
     print(line)
 

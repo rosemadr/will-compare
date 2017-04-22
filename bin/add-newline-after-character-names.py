68af9549e70ac651e98313ed4783b8634f3f67a3
rosemadr <madi@redpill.me.uk>
2017-04-22 10:41:42 +0100

    added complete character names

--- bin/add-newline-after-character-names.py
+++ bin/add-newline-after-character-names.py
@@ -5,7 +5,7 @@ import re
 
 for line in fileinput.input():
     # deal with abbreviated character names
-    for name in [ 'FRANCISCO', 'BERNARDO', 'MARCELLUS', 'HORATIO']:
+    for name in [ 'FRANCISCO', 'BERNARDO', 'MARCELLUS', 'HORATIO', 'KING CLAUDIUS', 'CORNELIUS', 'VOLTENMAND', 'LAERTES', 'POLONIUS', 'HAMLET', 'QUEEN GERTRUDE', 'OPHELIA', 'THE GHOST', 'REYNALDO', 'ROSENCRANTZ', 'GUILDENSTERN', 'FIRST PLAYER', 'PLAYER KING', 'PLAYER QUEEN', 'LUCIANUS', 'GRAVEDIGGER', 'OTHER']:
         line = re.sub('^'+name, name+'\n', line.rstrip())
 
     print(line)

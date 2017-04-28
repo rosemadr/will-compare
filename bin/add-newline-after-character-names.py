cb257c16d65de40297c1c949858f27b01932ea77
rosemadr <madi@redpill.me.uk>
2017-04-28 18:24:34 +0100

    updating char names
    dealing with lower case (‘in text’) name inconsistencies - Ghost,
    Ba/ernardo, Volti/enmand

--- bin/add-newline-after-character-names.py
+++ bin/add-newline-after-character-names.py
@@ -4,7 +4,6 @@ import fileinput
 import re
 
 for line in fileinput.input():
-    # deal with abbreviated character names
     for name in [ 'FRANCISCO', 'BERNARDO', 'MARCELLUS', 'HORATIO',
                   'KING', 'CORNELIUS', 'VOLTENMAND', 'LAERTES',
                   'POLONIUS', 'HAMLET', 'QUEEN', 'OPHELIA',
